c1fa11490a086067145e9d99eac4de25ac1fd6d7
virginiamo <scandarelli@hotmail.com>
2021-08-12 10:26:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const[countries, setCountries] = useState([]);
+useEffect(()=>{
+const getCountries = async()=>{
+ const response = await fetch('https://restcountries.eu/rest/v2/region/europe')
+ const jsonResponse = await response.json();
+ setCountries(jsonResponse);
+}
+getCountries();
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Countries Drop Down List </h1>
+      <select name="country" className="form">
+   {
+     countries.map((country, index)=>{
+return <option key={`${country.name}-${index} `}>{country.name}</option>
+     })
+   }
+     </select>
     </div>
   );
 }
